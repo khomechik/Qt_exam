b5432166c6c074b865e1558cf8a7c3cb5bc07eb9
khomechik <khomechik@gmail.com>
2022-07-09 03:21:19 +0300

    main_code

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import subprocess
+from time import ctime
 
 import PySide2
 from PySide2 import QtWidgets, QtCore, QtGui
-from PySide2.QtWidgets import QTableWidgetItem, QListWidgetItem
+from PySide2.QtWidgets import QTableWidgetItem
 
 from form_main import Ui_Form
 from form_tracert import Ui_Form as form_tracert
@@ -17,26 +18,32 @@ class MyWindow(QtWidgets.QWidget):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
 
+        self.ip_list_settings = QtCore.QSettings("Qt_exam")
+
         self.initUi()
         self.initWindows()
 
         self.initThreads()
 
+        self.loadIPList()
+
     def initWindows(self):
-         self.childWindow = Child()
-         self.childWindow_2 = Child_2()
+        self.tracertWindow = tracertWindow()
+        self.settingsWindow = settingsWindow()
 
-    def open_childWindow(self):
-        self.childWindow.show()
+        self.settingsWindow.signal_ip_list.connect(self.load_ip_list_from_settings)
 
-    def open_childWindow_2(self):
-        self.childWindow_2.show()
+    def open_tracertWindow(self):
+        self.tracertWindow.show()
+
+    def open_settingsWindow(self):
+        self.settingsWindow.show()
 
     def initThreads(self):
         self.tracertThread = TracertThread()
         self.pingThread = PingThread()
 
-        #init threads signals
+        # init threads signals
         self.tracertThread.mySignal.connect(self.tracertThreadSignal)
 
         self.pingThread.started.connect(self.pingThreadStarted)
@@ -47,12 +54,17 @@ class MyWindow(QtWidgets.QWidget):
         # init widget signals
         self.ui.pushButton_3.clicked.connect(self.OnPBTracerClicked)
 
-        self.ui.pushButton_4.clicked.connect(self.open_childWindow_2)
+        self.ui.pushButton_4.clicked.connect(self.open_settingsWindow)
 
         self.ui.pushButton.clicked.connect(self.OnPBStartPingClicked)
         self.ui.pushButton_2.clicked.connect(self.OnPBStopPingClicked)
 
-    #slots for tracert
+    def loadIPList(self):
+        ip_list = self.ip_list_settings.value("ip_list", [])
+        if ip_list:
+            self.load_ip_list_from_settings(ip_list)
+
+    # slots for tracert
 
     def OnPBTracerClicked(self) -> None:
         """
@@ -62,7 +74,7 @@ class MyWindow(QtWidgets.QWidget):
         text, ok = QtWidgets.QInputDialog.getText(self, "IP-address", "Введите IP")
         self.tracertThread.setIP(text)
         if ok:
-            self.open_childWindow()
+            self.open_tracertWindow()
             self.tracertThread.start()
 
     def tracertThreadSignal(self, values) -> None:
@@ -71,20 +83,21 @@ class MyWindow(QtWidgets.QWidget):
         :param values: данные, полученные от tracert
         :return: None
         """
-        self.childWindow.ui.plainTextEdit.setPlainText(values)
+        self.tracertWindow.ui.plainTextEdit.setPlainText(values)
 
-    #slots for ping
+    # slots for ping
 
     def OnPBStartPingClicked(self):
         """
         Слот для кнопки start потока self.pingThread
         :return: None
         """
-        text, ok = QtWidgets.QInputDialog.getText(self, "IP-address", "Введите IP")
-        self.pingThread.setIP(text)
-        if ok:
-            self.pingThread.start()
+        rows = self.ui.tableWidget.rowCount()
+        for row in range(rows):
+            ip_addr = self.ui.tableWidget.item(row, 0).text()
+            self.pingThread.ip = ip_addr
 
+        self.pingThread.start()
 
     def OnPBStopPingClicked(self):
         self.pingThread.status = False
@@ -92,7 +105,6 @@ class MyWindow(QtWidgets.QWidget):
     def pingThreadStarted(self):
         self.ui.pushButton.setEnabled(False)
         self.ui.pushButton_2.setEnabled(True)
-
 
     def pingThreadFinished(self):
         """
@@ -109,21 +121,26 @@ class MyWindow(QtWidgets.QWidget):
         :return: None
         """
         flag = "потеряно = 0"
-        i = self.ui.tableWidget.rowCount()
-        if flag in str(emit_value):
-            i += 1
-            self.ui.tableWidget.setItem(i, 1, QTableWidgetItem(self.pingThread.ip))
-            self.ui.tableWidget.setItem(i, 2, QTableWidgetItem("Доступен"))
-        else:
-            self.ui.tableWidget.setItem(i, 1, QTableWidgetItem(self.pingThread.ip))
-            self.ui.tableWidget.setItem(i, 2, QTableWidgetItem("Недоступен"))
-            self.ui.plainTextEdit.setPlainText(emit_value)
+        for row in range(self.ui.tableWidget.rowCount()):
+            if flag in str(emit_value):
+                self.ui.tableWidget.setItem(row, 1, QTableWidgetItem("Доступен"))
+            else:
+                self.ui.tableWidget.setItem(row, 1, QTableWidgetItem("Недоступен"))
+                self.ui.plainTextEdit.setPlainText(f"Ip-адрес {self.ui.tableWidget.item(row, 0).text()} недоступен. Время потери доступа: {ctime()}")
 
+    def load_ip_list_from_settings(self, emit_value):
+        self.ui.tableWidget.clear()
 
-    #Threads
+        self.ui.tableWidget.setRowCount(len(emit_value))
+
+        for row, elem in enumerate(emit_value):
+            self.ui.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(elem))
+
+    # Threads
+
 
 class TracertThread(QtCore.QThread):
-    Signal = QtCore.Signal(str)
+    mySignal = QtCore.Signal(str)
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -144,7 +161,7 @@ class TracertThread(QtCore.QThread):
             command = f"tracert {self.ip}"
             pr = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
             stdout, stderr = pr.communicate()
-            self.Signal.emit(str(stdout.decode("cp866", "ignore")))
+            self.mySignal.emit(str(stdout.decode("cp866", "ignore")))
 
 
 class PingThread(QtCore.QThread):
@@ -154,14 +171,6 @@ class PingThread(QtCore.QThread):
         super().__init__(parent)
         self.status = None
         self.ip = None
-
-    def setIP(self, ip):
-        """
-        Метод для установки ip-адреса
-        :param ip: значение ip-адреса
-        :return: None
-        """
-        self.ip = ip
 
     def run(self):
         self.status = True
@@ -173,7 +182,7 @@ class PingThread(QtCore.QThread):
 
 
 # Класс, создающий окно для вывода данных из tracert
-class Child(QtWidgets.QWidget):
+class tracertWindow(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -188,7 +197,8 @@ class Child(QtWidgets.QWidget):
 
 
 # Класс, создающий окно settings
-class Child_2(QtWidgets.QWidget):
+class settingsWindow(QtWidgets.QWidget):
+    signal_ip_list = QtCore.Signal(list)
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -200,18 +210,53 @@ class Child_2(QtWidgets.QWidget):
 
         self.initUi()
 
-    def initUi(self): #НЕ РАБОТАЕТ
-        ip_list = self.ip_list_settings.value("ip_list", [])  # ошибка EOFError: Ran out of input
-        i = 0
+        self.load_ip()
+
+    def initUi(self):
+        self.ui.pushButton.clicked.connect(self.add_ip)
+        self.ui.pushButton_2.clicked.connect(self.delete_ip)
+
+    def add_ip(self):
+        text, ok = QtWidgets.QInputDialog.getText(self, "ip", "ip")
+
+        if not ok:
+            return
+        for symbol in text:
+            if symbol.isalpha():
+                QtWidgets.QMessageBox.critical(self, "Ошибка!!!", "Неверный ip-адрес!", QtWidgets.QMessageBox.Ok)
+                return
+
+        self.ui.listWidget.addItem(text)
+
+    def delete_ip(self):
+        text, ok = QtWidgets.QInputDialog.getText(self, "ip", "удалить ip-адрес")
+
+        if not ok:
+            return
+
+        for row in range(self.ui.listWidget.count()):
+            if text == self.ui.listWidget.item(row).text():
+                self.ui.listWidget.takeItem(row)
+
+    def load_ip(self):
+        ip_list = self.ip_list_settings.value("ip_list", [])
         if ip_list:
-            self.ui.listWidget.addItem(QListWidgetItem(ip_list[i]))
-            i += 1
+            for ip_addr in ip_list:
+                self.ui.listWidget.addItem(ip_addr)
+
+    def get_ip_list(self):
+        result = []
+
+        for i in range(self.ui.listWidget.count()):
+            result.append(self.ui.listWidget.item(i).text())
+
+        return result
 
     def closeEvent(self, event: PySide2.QtGui.QCloseEvent) -> None:
-        if self.ui.listWidget.count():
-            rows = self.ui.listWidget.count()
-            for i in range(0, rows):
-                self.ip_list_settings.setValue("ip_list", [self.ui.listWidget.item(i)])
+
+        ip_list = self.get_ip_list()
+        self.signal_ip_list.emit(ip_list)
+        self.ip_list_settings.setValue("ip_list", ip_list)
 
 
 if __name__ == "__main__":
